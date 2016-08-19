5bb7f41f34fdbb3758b44b44f5420d796b147117
William Song <ksskor@falinux.com>
2016-08-19 07:56:52 +0000

    backup

--- install_cmd.sh
+++ install_cmd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo cp -a gulp-tool       /usr/bin/
-sudo cp -a gulp-tool-dev   /usr/bin/
+sudo cp -a gulp-tool			/usr/bin/
+sudo cp -a gulp-tool-dev		/usr/bin/
+sudo cp -a gulp-ddns			/usr/bin/
 
